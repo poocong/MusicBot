c837a50b7ae95f99d9e3b00fbbb78b1259c69e8a
rzkyfadilla._ <81405011+poocong@users.noreply.github.com>
2022-02-01 23:02:48 +0700

    Create ping.py

---
+++ plugins/ping.py
@@ -0,0 +1,18 @@
+from pyrogram import filters, Client
+
+from pyrogram.types import Message
+
+import os
+
+import shutil
+
+import psutil
+
+from sys import version
+
+import time
+
+from utils.sys_stats import humanbytes
+
+from datetime import datetime
+
