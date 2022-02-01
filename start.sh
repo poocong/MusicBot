6de79bb443484c121428b7b509a54282e6bec7f5
rzkyfadilla._ <81405011+poocong@users.noreply.github.com>
2022-02-01 22:49:25 +0700

    .

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/poocong/MusicBot /MusicBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/poocong/MusicBot -b $BRANCH /MusicBot
 fi
-cd /VCPlayerBot
+cd /MusicBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
